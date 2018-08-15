ffe4151ffe00f50ea4a2c3cec9533de447cc6a80
Guicai Ma <linux.sir@qq.com>
2018-08-15 15:06:03 +0000

    Debug Hello World in Assemble

---
+++ Part14/example1.cpp
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  example1.cpp
+ *
+ *    Description:  hello world For ARM Reverse Engineer
+ *
+ *        Version:  1.0
+ *        Created:  2018年08月15日 22时14分14秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
+
